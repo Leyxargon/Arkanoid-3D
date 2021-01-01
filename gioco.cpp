6b510117b4f02da3ea1a3a1520dc40c268f25036
Leyxargon <43447240+Leyxargon@users.noreply.github.com>
2021-01-01 15:11:30 +0100

    Update gioco.cpp

--- gioco.cpp
+++ gioco.cpp
@@ -5,7 +5,6 @@ Gioco::Gioco(int argc, char **argv) {
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowPosition(50, 50);
 	glutInitWindowSize(LARGHEZZA_FINESTRA, ALTEZZA_FINESTRA);
-	//glEnable(GL_DEPTH_TEST);
 }
 
 void Gioco::esegui() {
